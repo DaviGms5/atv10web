0804b6305b28843c6f917a570f821f116e9ab049
DaviGms5 <132697298+DaviGms5@users.noreply.github.com>
2025-08-15 02:40:40 +0000

    Algumas atualizações no js, nas paginas html

---
+++ js/main.js
@@ -0,0 +1,24 @@
+botaoDeletar = document.getElementById("botao-deletar");
+botaoCompradores = document.getElementById("botao-compradores");
+botaoEditar = document.getElementById("botao-editar");
+botaoLogin = document.getElementById("botao-login");
+
+function botaoEditar()
+{
+
+}
+
+function botaoDeletar()
+{
+    
+}
+
+function botaoCompradores()
+{
+    
+}
+
+function botaoLogin()
+{
+
+}
