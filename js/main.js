1880e2eaec8d54614e13f73b06f1b69f624006b4
DaviGms5 <132697298+DaviGms5@users.noreply.github.com>
2025-08-16 16:47:47 +0000

    Mudança nos produtos através de um grid permitindo flexibilidade entre computador, celular ...., adicionando funcionalidade a tela de adm

--- js/main.js
+++ js/main.js
@@ -18,7 +18,21 @@ function botaoCompradores()
     
 }
 
-function botaoLogin()
-{
+document.addEventListener("DOMContentLoaded", () => {
+    const formulario = document.getElementById("formulario-entrada");
 
-}
+    formulario.addEventListener("submit", (event) => {
+        event.preventDefault(); // impede o reload da página
+
+        const usuario = document.getElementById("nomeUsuario").value.trim();
+        const senha = document.getElementById("senhaUsuario").value.trim();
+
+        if (usuario === "Admin123!" && senha === "Admin123!") {
+            // Redireciona para a página de administração
+            window.location.href = "paginaAdministracao.html";
+        } else {
+            alert("A senha está incorreta! Volte caso queira entrar como usuário");
+        }
+    });
+});
+
